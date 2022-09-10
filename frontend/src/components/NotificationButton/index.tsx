2acae68e91cf87e1156c7793fb26c3747967c1df
Davi <davi_martins2009@outlook.com>
2022-09-10 19:29:13 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -7,10 +8,10 @@ import './styles.css'
 type Props = {
   saleId:number;
 }
-function handleClick(id:number){
+function handleClick(id :number){
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response => {
-    console.log("Sucesso!")
+    toast.info("SMS enviado com sucesso")
   })
 }
 
